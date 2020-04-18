8de193d2bddf9a27955fcfdcd084e92fc00e8f24
Andrew Lee <andrewimg@gmail.com>
2020-04-17 18:04:49 -0700

    created for loops to return correct amount of results

--- liri.js
+++ liri.js
@@ -1,61 +1,50 @@
 var keys = require("./keys.js");
 
 var Spotify = require('node-spotify-api');
- 
+
 var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 
 var input = process.argv[2];
 var searchTerm = process.argv.slice(3).join(' ')
 console.log(searchTerm)
+
 var fs = require('fs')
 
 function spotifySearch() {
-    spotify.search({ type: 'track', query: searchTerm }, function(err, response) {
+    spotify.search({ type: 'track', query: searchTerm }, function (err, response) {
         if (err) {
-          return console.log('Error occurred: ' + err);
+            return console.log('Error occurred: ' + err);
         }
-       
-      console.log(response.tracks.items); 
-      });
+        for (i = 0; i < response.tracks.items.length; i++) {
+            console.log(response.tracks.items);
+        }
+    });
 }
 function concertSearch() {
     axios.get("https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=codingbootcamp")
-    .then(function(response){
-        console.log(response.data)
-    })
+        .then(function (response) {
+            // console.log(response.data)
+            for (i = 0; i < response.data.length; i++) {
+                console.log(response.data)
+            }
+        })
 }
 function movieSearch() {
     axios.get("http://www.omdbapi.com/?t=" + searchTerm + "&y=&plot=short&apikey=trilogy").then(
-  function(response) {
-    console.log("The movie's rating is: " + response.data.imdbRating);
-    console.log(response.data)
-  })
-  .catch(function(error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log("---------------Data---------------");
-      console.log(error.response.data);
-      console.log("---------------Status---------------");
-      console.log(error.response.status);
-      console.log("---------------Status---------------");
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an object that comes back with details pertaining to the error that occurred.
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log("Error", error.message);
-    }
-    console.log(error.config);
-  });
-
+        function (response) {
+            console.log("Release Date: " + response.data.Released);
+            console.log("The movie's IMDB rating is: " + response.data.imdbRating);
+            console.log("The movie's Rotten Tomatoes rating is: " + response.data.Ratings[1].Value)
+            console.log("Release Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+        })
 }
 
-function readRandom(){
-    fs.readFile("./random.txt", "utf8", function(err, data){
+function readRandom() {
+    fs.readFile("./random.txt", "utf8", function (err, data) {
         console.log(data.split(","))
         var fileDataArray = data.split(",")
         input = fileDataArray[0]
@@ -66,23 +55,23 @@ function readRandom(){
 
 
 function menu() {
-switch (input) {
-    case "concert-this":
-        concertSearch()
-        break;
-    case "spotify-this-song":
-        spotifySearch()
-        // process.argv[3] = song-search
-        break;
-    case "movie-this":
-        movieSearch()
-        break;
-    case "do-what-it-says":
-        readRandom()
-        break;
-};
+    switch (input) {
+        case "concert-this":
+            concertSearch()
+            break;
+        case "spotify-this-song":
+            spotifySearch()
+            // process.argv[3] = song-search
+            break;
+        case "movie-this":
+            movieSearch()
+            break;
+        case "do-what-it-says":
+            readRandom()
+            break;
+    };
 }
 menu();
 
 
- 
+
